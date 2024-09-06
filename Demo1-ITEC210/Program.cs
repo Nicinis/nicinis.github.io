d2deff5eb4cfdade2aad151e20f75bfc039ece02
Nicinis <nicholas.zadorozn@mymhc.ca>
2024-09-06 14:42:07 -0600

    Testing changes for publishing the website through gitHub.

--- Demo1-ITEC210/Program.cs
+++ Demo1-ITEC210/Program.cs
@@ -2,6 +2,7 @@ using Demo1_ITEC210;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+//comment for testing
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
